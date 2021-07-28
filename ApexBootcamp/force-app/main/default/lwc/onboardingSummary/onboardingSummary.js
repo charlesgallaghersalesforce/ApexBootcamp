2f3dfda1c8d5a68732655336938520d1ec5b2988
Alejandro Moreno <amorenoo@me.com>
2021-07-28 17:44:26 -0500

    Updated onboarding summary lwc

--- ApexBootcamp/force-app/main/default/lwc/onboardingSummary/onboardingSummary.js
+++ ApexBootcamp/force-app/main/default/lwc/onboardingSummary/onboardingSummary.js
@@ -1,64 +1,65 @@
-import { LightningElement, wire, api, track } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import getOnboardingSummary from '@salesforce/apex/OnboardingController.getOnboardingSummary';
 
 export default class OnboardingSummary extends LightningElement {
     @api recordId;
-    loaded = false;
-    planName;
-    startDate;
-    endDate;
-    status;
-    aheadBehind;
-    weekNo;
-    achieved;
-    target;
-    currentStage;
-    progress;
-    planStages = [];
+    isLoading;
+    onboardingData;
+    progressActual;
+    progressTarget;
 
-    @wire(getOnboardingSummary, {recordId: '$recordId'})
-    wiredPlan({ error, data }) {
-        if (data) {
-            this.loaded = true;
-            this.planName = data.name;
-            var planStartDate = new Date(data.startDate);
-            var planStartDay = planStartDate.getDate() + 1;
-            var planStartMonth = planStartDate.getMonth() + 1;
-            this.startDate = planStartMonth + '/' + planStartDay + '/' + planStartDate.getFullYear();
-            var planEndDate = new Date(data.endDate);
-            var planEndDay = planEndDate.getDate() + 1;
-            var planEndMonth = planEndDate.getMonth() + 1;
-            this.endDate = planEndMonth + '/' + planEndDay + '/' + planStartDate.getFullYear();
-            this.status = data.status;
-            this.aheadBehind = data.aheadBehind;
-            this.weekNo = data.weekNo;
-            this.achieved = data.achieved;
-            this.target = data.target;
-            this.currentStage = data.currentStage;
-            if (data.target == 0) {
-                this.progress = 0;
-            } else {
-                var progressTmp = (data.achieved / data.target) * 100;
-                this.progress = progressTmp.toFixed(0);
-            }
-            this.planStages = data.planStages;
-            this.error = undefined;
-        } else if (error) {
-            this.error = error;
-            this.planName = undefined;
+    isRendered = false;
+    renderedCallback() {
+        if (!this.isRendered) {
+            this.isRendered = true;
+            this.getOnboardingData();
         }
     }
 
+    async getOnboardingData() {
+        try {
+            this.handleLoading();
+            const result = await getOnboardingSummary({recordId: this.recordId});
+            let responseWrapper = JSON.parse(result);
+            this.onboardingData = responseWrapper.data;
+            let startDate = new Date(this.onboardingData.startDate).toLocaleDateString();
+            this.onboardingData.startDate = startDate;
+            let endDate = new Date(this.onboardingData.endDate).toLocaleDateString();
+            this.onboardingData.endDate = endDate;
+            this.progressActual = ((this.onboardingData.achieved / this.onboardingData.totalOnboarding) * 100).toFixed(0);
+            this.progressTarget = ((this.onboardingData.target / this.onboardingData.totalOnboarding) * 100).toFixed(0);
+
+            if (!responseWrapper.status || !responseWrapper.data) {
+                console.log(`There was an issue retrieving onboarding data ${responseWrapper.status}`);
+            }
+
+        } catch(error) {
+            console.log(`There was an issue retrieving onboarding data ${error}`);
+        } finally {
+            this.handleDoneLoading();
+        }
+    }
+
+    // Handles loading event
+    handleLoading() { 
+        this.isLoading = true;
+    }
+    
+    // Handles done loading event
+    handleDoneLoading() {
+        this.isLoading = false;
+    }
+
     get isFarBehind() {
-        return this.status == 'Far Behind' ? true : false;
+        return this.onboardingData.status == 'Far Behind' ? true : false;
     }
     get isSlightlyBehind() {
-        return this.status == 'Slightly Behind' ? true : false;
+        return this.onboardingData.status == 'Slightly Behind' ? true : false;
     }
     get isOnPace() {
-        return this.status == 'On Pace' ? true : false;
+        return this.onboardingData.status == 'On Pace' ? true : false;
     }
     get isAheadOfPace() {
-        return this.status == 'Ahead of Pace' ? true : false;
+        return this.onboardingData.status == 'Ahead of Pace' ? true : false;
     }
 }
